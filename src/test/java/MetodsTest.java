f6a4984dc4391cc2f1189efa73381456a6810a11
jpraj <TUgit2020!>
2021-05-20 21:12:58 +0200

    initial tests of methods

--- src/test/java/MetodsTest.java
+++ src/test/java/MetodsTest.java
@@ -1,2 +1,31 @@
+import org.junit.*;
+
 public class MetodsTest {
+    @Test
+    public void testMinimum()
+    {
+        Metods metods = new Metods();
+        Assert.assertEquals(metods.minimum(3,9), 3);
+    }
+
+    @Test
+    public void testMaximum()
+    {
+        Metods metods = new Metods();
+        Assert.assertEquals(metods.maximum(75, -8), 75);
+    }
+
+    @Test
+    public void testIsPositiveTrue()
+    {
+        Metods metods = new Metods();
+        Assert.assertEquals(metods.isPositive(99), true);
+    }
+
+    @Test
+    public void testIsPositiveFalse()
+    {
+        Metods metods = new Metods();
+        Assert.assertEquals(metods.isPositive(-23), false);
+    }
 }
