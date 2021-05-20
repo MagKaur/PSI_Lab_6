8b6760231032dffc71c3859d18d5ca3bba20bb96
MagKaur <kochanowska.magd@gmail.com>
2021-05-20 21:32:05 +0200

    Finished methods

--- src/main/java/Metods.java
+++ src/main/java/Metods.java
@@ -1,2 +1,43 @@
 public class Metods {
+
+
+    public int minimum (int x, int y){
+
+        int min = 0;
+
+        if (x>y){
+            min = y;
+        }else{
+            min = x;
+        }
+
+        return min;
+    }
+
+
+    public int maximum (int x, int y){
+
+        int max = 0;
+
+        if (x>y){
+
+            max = x;
+        }else{
+            max = y;
+        }
+
+        return max;
+    }
+
+    public boolean isPositive (int x){
+
+        boolean result = false;
+
+        if (x>0){
+
+            result = true;
+        }
+
+        return result;
+    }
 }
